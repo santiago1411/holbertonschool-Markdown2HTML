4e22b3c31224d8ecd44d958c31547160897c9b60
santiago1411 <santy.osorno2@gmail.com>
2023-04-06 22:04:38 -0500

    mark

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-from sys import argv, stderr
-import os
+""" Write a script markdown2html.py that takes an argument 2 strings:
+First argument is the name of the Markdown file
+Second argument is the output file name """
+
 import re
 import hashlib
+import sys
+import os
 
-def markdown2html():
-    """main program"""
-    if len(argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
-    if not os.path.exists(argv[1]):
-        print(f"Missing {argv[1]}", file=stderr)
+    if not os.path.exists(sys.argv[1]):
+        sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit(1)
 
-    with open(argv[1]) as r:
-        with open(argv[2], 'w') as w:
+    with open(sys.argv[1]) as r:
+        with open(sys.argv[2], 'w') as w:
             change_status = False
             ordered_status = False
             paragraph = False
@@ -87,6 +90,3 @@ def markdown2html():
                 w.write('</p>\n')
 
     exit(0)
-
-if __name__ == "__main__":
-    markdown2html()
